3dfde39e5e6639ad003559d95c0ec8205a4d95ff
Hemesh Patel <hemeshpatel@hotmail.co.uk>
2019-06-21 07:21:03 +0100

    Add files via upload

---
+++ sql_statements/booking_dump.sql
@@ -0,0 +1,24 @@
+CREATE TABLE public.booking_data
+    (saleId text NOT NULL
+    ,saleName text
+    ,offerName text
+    ,departureAirportCode text
+    ,departureAirportName text
+    ,adults text
+    ,children text
+    ,infants text
+    ,county text
+    ,checkIn text
+    ,checkOut text
+    ,rooms text
+    ,currency text
+    ,type text
+    ,transactionId text
+    ,destinationName text
+    ,destinationType text
+    ,country text
+    ,division text
+    ,city text
+    ,providerName text
+    ,platformName text
+    )
---
+++ sql_statements/sales_dump.sql
@@ -0,0 +1,30 @@
+CREATE TABLE public.sales_data
+    (
+      id text NOT NULL
+      ,title text
+      ,sale_title text
+      ,start text
+      ,eend text
+      ,url text
+      ,destination_type text
+      ,destination_name text
+      ,rate text
+      ,defaultCurrencyRate text
+      ,discount text
+      ,discount_value text
+      ,description text
+      ,image text
+      ,image_thumb text
+      ,image_medium text
+      ,county text
+      ,country text
+      ,division text
+      ,city text
+      ,city_district text
+      ,product_type text
+      ,travel text
+      ,highlights text
+      ,category text
+      ,images text
+      ,price_details text
+    )
